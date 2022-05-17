bf8d3cc4847f7bcc4ab8dfa9513e92c6341763c8
Valarie Chelagat <valarie.chelagat@student.moringaschool.com>
2022-05-17 08:21:06 -0400

    created user test

--- app/models.py
+++ app/models.py
@@ -1,4 +1,5 @@
 from . import db
+from werkzeug.security import generate_password_hash,check_password_hash
 
 
 
@@ -51,6 +52,21 @@ class User(db.Model):
 
     def __repr__(self):
         return f'User {self.username}'
+
+    pass_secure  = db.Column(db.String(255))
+
+    @property
+    def password(self):
+            raise AttributeError('You cannot read the password attribute')
+
+    @password.setter
+    def password(self, password):
+            self.pass_secure = generate_password_hash(password)
+
+
+    def verify_password(self,password):
+            return check_password_hash(self.pass_secure,password)
+
 
 class Role(db.Model):
     __tablename__ = 'roles'
