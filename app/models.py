e0a91f4d7c76b4998498f138443cbc6d6f9b624b
Valarie Chelagat <valarie.chelagat@student.moringaschool.com>
2022-05-17 07:36:22 -0400

    added users and role

--- app/models.py
+++ app/models.py
@@ -1,13 +1,7 @@
 from . import db
 
 
-class User(db.Model):
-    __tablename__ = 'users'
-    id = db.Column(db.Integer,primary_key = True)
-    username = db.Column(db.String(255))
 
-    def __repr__(self):
-        return f'User {self.username}'
 
 class Quotes:
     '''
@@ -46,3 +40,20 @@ class Post:
                 response.append(post)
 
         return response
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer,primary_key = True)
+    username = db.Column(db.String(255))
+
+    def __repr__(self):
+        return f'User {self.username}'
+
+class Role(db.Model):
+    __tablename__ = 'roles'
+
+    id = db.Column(db.Integer,primary_key = True)
+    name = db.Column(db.String(255))
+
+    def __repr__(self):
+        return f'User {self.name}'
