a855851b782088f6f4d65aaa64fc0fb86bf15b84
Valarie Chelagat <valarie.chelagat@student.moringaschool.com>
2022-05-18 07:16:21 -0400

    image uploads

--- app/main/forms.py
+++ app/main/forms.py
@@ -7,3 +7,7 @@ class PostForm(FlaskForm):
     author = StringField('Author',validators=[DataRequired()])
     quote = TextAreaField('Quote', validators=[DataRequired()])
     submit = SubmitField('Submit')
+
+class UpdateProfile(FlaskForm):
+    bio = TextAreaField('Tell us about you.',validators = [DataRequired()])
+    submit = SubmitField('Submit')
--- config.py
+++ config.py
@@ -10,6 +10,7 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
     QUOTES_BASE_URL =' http://quotes.stormconsultancy.co.uk/popular.json'
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://valarie:1033927@localhost/blog'
+    UPLOADED_PHOTOS_DEST ='app/static/photos'
 
 class ProdConfig(Config):
     pass
